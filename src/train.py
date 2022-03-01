0869e53a492efd6042bffa0d02b66a9afac65e9e
atomic-rooster <chaitanya.7.kapoor@gmail.com>
2022-03-01 14:18:25 +0530

    finished training script

--- src/train.py
+++ src/train.py
@@ -9,17 +9,41 @@ import torchvision
 import torchvision.transforms as transforms
 import os
 import datetime
+import argparse
+from dataloader import Dgaze_DataSet
 from tqdm import tqdm
 from torch.nn.functional import normalize
 from torchsummary import summary
+from network import *
 
 
-optimizer = optim.Adam(model.parameters(), lr=lr, eps=eps)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [40,55], gamma=gamma)
-loss_fn = nn.L1Loss()
+# command line arguments
+def get_args():
+   parser = argparse.ArgumentParser(description='PyTorch Gaze prediction')
+   parser.add_argument('--n_epochs', type=int, default=60,
+                       help='number of training epochs')
+   parser.add_argument('--lr', type=int, default=1e-5,
+                       help='learning rate')
+   parser.add_argument('--eps', type=int, default=1e-5)
+   parser.add_argument('--gamma', type=int, default=0.1, help='multi-step LR weight decay')
+   parser.add_argument('--dest_dir', type=str, help='destination directory to save weights')
+   parser.add_argument('--num_channels', type=int, default=3,
+                       help='number of input channels')
+   parser.add_argument('--num_features', type=int, default=32,
+                       help='channel size')
+   parser.add_argument('--growth_rate', type=int, default=32,
+                       help='growth rate')
+   parser.add_argument('--num_blocks', type=int, default=4,
+                       help='number of residual dense blocks')
+   parser.add_argument('--num_layers', type=int, default=4,
+                       help='number of residual layers')
+
+   args = parser.parse_args()
+   return args
 
 
-def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler):
+# training loop
+def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler, dest_dir):
     for epoch in range(1, n_epochs + 1):
         loss_train = 0.0
         loop = tqdm(enumerate(train_loader), total=len(train_loader))
@@ -40,13 +64,27 @@ def train(n_epochs, optimizer, model, loss_fn, train_loader, scheduler):
             loop.set_postfix(loss = loss.item())
             loss_train += loss.item()
         scheduler.step()
-        torch.save(model.state_dict(), '/content/drive/MyDrive/DGAZE/saved_weights/final_network.pth')
+        torch.save(model.state_dict(), dest_dir)
 
 
+def main():
+    args = get_args()
 
-train(n_epochs=n_epochs,
-      optimizer=optimizer,
-      model=model,
-      loss_fn=loss_fn,
-      train_loader=train_loader,
-      scheduler=scheduler)
+    model = DRGaze(num_channels=args.num_channels, num_features=args.num_features, growth_rate=args.growth_rate,
+                   num_blocks=args.num_blocks, num_layers=args.num_layers)
+    optimizer = optim.Adam(model.paramters(), lr=args.lr, eps=args.eps)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [40, 55], gamma=args.gamma)
+    loss_fn = nn.L1Loss()
+    train_loader = Dgaze_DataSet()
+
+    train(n_epochs=args.n_epochs,
+          optimizer=optimizer,
+          model=model,
+          loss_fn=loss_fn,
+          train_loader=train_loader,
+          scheduler=scheduler,
+          dest_dir=args.dest_dir)
+
+
+if __name__ == '__main__':
+    main()
